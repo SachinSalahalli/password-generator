d4dcbf0fa345cc219e7baded8519652878834849
Sachin Salahalli <130772723+SachinSalahalli@users.noreply.github.com>
2025-04-01 17:51:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/PASSWORDGENERATOR/", // 👈 Add your repo name here
+  base: "/password-generator/", 
 });
